2de26fa9c9fdc7980cee6c58c3804def779ef228
wicahma <teguh.dc.kusuma@gmail.com>
2024-08-18 18:00:58 +0700

    fix: mysql2 vercel error

--- src/models/index.js
+++ src/models/index.js
@@ -1,34 +1,49 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const mysql2 = require("mysql2");
 
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 // eslint-disable-next-line import/no-dynamic-require
 const config = require(`${__dirname}/../config/database.js`)[env];
 const db = {};
 
+if (config.dialect === "mysql") {
+  config.dialectModule = mysql2;
+}
+
 let sequelize;
 if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-    sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
 fs.readdirSync(__dirname)
-    .filter((file) => {
-        return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
-    })
-    .forEach((file) => {
-        // eslint-disable-next-line global-require,import/no-dynamic-require
-        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-        db[model.name] = model;
-    });
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach((file) => {
+    // eslint-disable-next-line global-require,import/no-dynamic-require
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
+    db[model.name] = model;
+  });
 
 Object.keys(db).forEach((modelName) => {
-    if (db[modelName].associate) {
-        db[modelName].associate(db);
-    }
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
 db.sequelize = sequelize;
