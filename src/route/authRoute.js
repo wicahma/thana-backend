7cad883c1238e1ec72f0beccfee26c9ee40df2db
wicahma <teguh.dc.kusuma@gmail.com>
2024-08-19 18:27:10 +0700

    update: adding new route into index route handler

--- src/route/authRoute.js
+++ src/route/authRoute.js
@@ -1,23 +1,31 @@
-const express = require('express');
-const AuthController = require('../controllers/AuthController');
-const UserValidator = require('../validator/UserValidator');
+const express = require("express");
+const AuthController = require("../controllers/AuthController");
+const UserValidator = require("../validator/UserValidator");
 
 const router = express.Router();
-const auth = require('../middlewares/auth');
+const auth = require("../middlewares/auth");
 
 const authController = new AuthController();
 const userValidator = new UserValidator();
 
-router.post('/register', userValidator.userCreateValidator, authController.register);
-router.post('/email-exists', userValidator.checkEmailValidator, authController.checkEmail);
-router.post('/login', userValidator.userLoginValidator, authController.login);
-router.post('/refresh-token', authController.refreshTokens);
-router.post('/logout', authController.logout);
+router.post(
+  "/register",
+  userValidator.userCreateValidator,
+  authController.register
+);
+router.post(
+  "/email-exists",
+  userValidator.checkEmailValidator,
+  authController.checkEmail
+);
+router.post("/login", userValidator.userLoginValidator, authController.login);
+router.post("/refresh-token", authController.refreshTokens);
+router.post("/logout", authController.logout);
 router.put(
-    '/change-password',
-    auth(),
-    userValidator.changePasswordValidator,
-    authController.changePassword,
+  "/change-password",
+  auth(),
+  userValidator.changePasswordValidator,
+  authController.changePassword
 );
 
 module.exports = router;
--- src/route/index.js
+++ src/route/index.js
@@ -1,17 +1,32 @@
-const express = require('express');
-const authRoute = require('./authRoute');
+const express = require("express");
+const authRoute = require("./authRoute");
+const assetRoute = require("./assetRoute");
+const kecamatanRoute = require("./kecamatanRoute");
+const skpdRoute = require("./skpdRoute");
 
 const router = express.Router();
 
 const defaultRoutes = [
-    {
-        path: '/auth',
-        route: authRoute,
-    },
+  {
+    path: "/auth",
+    route: authRoute,
+  },
+  {
+    path: "/aset",
+    route: assetRoute,
+  },
+  {
+    path: "/kecamatan",
+    route: kecamatanRoute,
+  },
+  {
+    path: "/skpd",
+    route: skpdRoute,
+  },
 ];
 
 defaultRoutes.forEach((route) => {
-    router.use(route.path, route.route);
+  router.use(route.path, route.route);
 });
 
 module.exports = router;
