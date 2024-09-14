1d87fa86c2b6b7e9d0f75c96077bfd4b24b4e1c0
wicahma <teguh.dc.kusuma@gmail.com>
2024-09-14 14:34:01 +0700

    feat: adding file handling middleware

---
+++ src/middlewares/fileHandler.js
@@ -0,0 +1,58 @@
+const multer = require("multer");
+const fs = require("fs");
+
+const fileHandler = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      if (file.mimetype.includes("image")) {
+        if (!fs.existsSync(`${__dirname}/../../public/images`)) {
+          fs.mkdirSync(`${__dirname}/../../public/images`, { recursive: true });
+        }
+        cb(null, `${__dirname}/../../public/images`);
+      } else if (file.mimetype.includes("pdf")) {
+        if (!fs.existsSync(`${__dirname}/../../public/docs`)) {
+          fs.mkdirSync(`${__dirname}/../../public/docs`, { recursive: true });
+        }
+        cb(null, `${__dirname}/../../public/docs`);
+      }
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + "_" + file.originalname);
+    },
+  }),
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype.includes("image") ||
+      file.mimetype.includes("msword") ||
+      file.mimetype.includes("pdf") ||
+      file.mimetype.includes(
+        "vnd.openxmlformats-officedocument.wordprocessingml.document"
+      ) ||
+      file.mimetype.includes("vnd.ms-excel") ||
+      file.mimetype.includes(
+        "vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+      )
+    ) {
+      cb(null, true);
+    } else {
+      cb(new Error("File harus berupa gambar / dokumen!"), false);
+    }
+  },
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+const deleteFile = (filePath) => {
+  if (!fs.existsSync(filePath))
+    return "Image File not found, but data was deleted!";
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.log(err);
+      return `Internal Error when deleting file: ${err}`;
+    } else {
+      console.log("File deleted");
+      return "Image File deleted succesfully";
+    }
+  });
+};
+
+module.exports = { fileHandler, deleteFile };
